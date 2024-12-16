175d1d5cb1f3b78a1f12e7814c2c5041c25c8aba
lanl2tz <lanl2tz@hotmail.com>
2024-12-17 00:05:07 +0800

    antialiasing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <raylib.h>
 
 int main() {
-  InitWindow(300, 600, "Simulator");
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+
+  // Enable Multi-Sample Anti-Aliasing (MSAA) before window creation
+  InitWindow(300, 300, "Simulator");
+
   SetTargetFPS(60);
+
   while (WindowShouldClose() == false) {
     BeginDrawing();
-    
+      ClearBackground(RAYWHITE);
+      DrawCircle(150, 150, 50, BLUE); 
     EndDrawing();
   }
   
